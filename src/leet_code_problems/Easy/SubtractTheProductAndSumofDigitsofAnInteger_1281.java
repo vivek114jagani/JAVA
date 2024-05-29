31ea24ffee91372503e99f4c98eb09ef64829761
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2024-05-29 17:06:42 +0530

    file updated

--- src/leet_code_problems/Easy/SubtractTheProductAndSumofDigitsofAnInteger_1281.java
+++ src/leet_code_problems/Easy/SubtractTheProductAndSumofDigitsofAnInteger_1281.java
@@ -5,23 +5,48 @@ package leet_code_problems.Easy;
 public class SubtractTheProductAndSumofDigitsofAnInteger_1281 {
     public static void main(String[] args) {
         int n = 234;
-        System.out.println(subtractProductAndSum(n));
+        System.out.println(subtractProductAndSum(n));   // 15
 
         int m = 4421;
-        System.out.println(subtractProductAndSum(m));
+        System.out.println(subtractProductAndSum(m));   // 21
     }
 
     public static int subtractProductAndSum(int n) {
-        int multi = 1;
-        int sum = 0;
-        int rem;
 
+        // n = 234  // 23   // 2    // 0
+
+        int multi = 1; // 4   // 12   // 24
+        int sum = 0; // 4   // 7    // 9
+        int rem; // 4   // 3    // 2
+
+        // 1) n=234 > 0  ==  true
+        // 2) n=23 > 0  ==  true
+        // 3) n=2 > 0  ==  true
+        // 4) n=0 > 0  ==  fale
         while(n > 0) {
+
+            // 1) n=234 % 10  ==  rem=4
+            // 2) n=23 % 10  ==  rem=3
+            // 3) n=2 % 10  ==  rem=2
             rem = n % 10;
+
+            // 1) multi=1 * rem=4  ==  multi=4
+            // 2) multi=4 * rem=3  ==  multi=12
+            // 3) multi=12 * rem=2  ==  multi=24
             multi *= rem;
+
+            //1) sum=0 + rem=4  ==  sum=4
+            //2) sum=4 + rem=3  ==  sum=7
+            //3) sum=7 + rem=2  ==  sum=9
             sum += rem;
+
+            // 1) n=234 / 10  ==  n=23
+            // 2) n=23 / 10  ==  n=2
+            // 3) n=2 / 10  ==  n=0
             n /= 10;
         }
+
+        // 4) multi=24 - sum=9  ==  return 24-9 = 15
         return multi - sum;
     }
 }
